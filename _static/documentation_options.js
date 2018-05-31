298692df2e3f4f40236e07b97dc72c7c71ee09fa
Travis CI User <travis@example.org>
2018-05-31 12:33:22 +0000

    Update documentation

---
+++ _static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: '0.6.0.post3+git9615862',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
